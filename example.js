26962bb87beb1bfcfc667645cb26cb8dddc65395
Alfredo Vecino <fredovecino@gmail.com>
2019-04-01 00:50:36 -0400

    Starting rule system..

---
+++ example.js
@@ -0,0 +1,5 @@
+if(branchData.length == 1){populateProfileSelect();}
+# comment line 1
+option1: value1
+option2: value2
+if(branchData.length == 1){populateProfileSelect();}
--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -23,11 +27,44 @@ func main() {
 	// Serve static files from public directory
 	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./public/"))))
 
+	checkFile("example.js")
+
 	log.Println("Starting server on port " + *port)
-	log.Fatal(http.ListenAndServe(":"+*port, router))
+	//log.Fatal(http.ListenAndServe(":"+*port, router))
 
 }
 
 func testHandle(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Ok")
+}
+
+func checkFile(filename string) {
+	extension := strings.Split(filename, ".")[1]
+
+	regexpFile, err := ioutil.ReadFile("rules/" + extension + ".rule")
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	log.Println(findLine(content, 21))
+	re := regexp.MustCompile(string(regexpFile))
+	log.Println(re.FindAllIndex([]byte(content), -1))
+	log.Println(re.FindAllStringIndex(string(content), -1))
+}
+
+func findLine(str []byte, index int) int {
+	cont := 1
+	for i := 0; i < index; i++ {
+		if str[i] == 10 {
+			cont++
+		}
+	}
+	return cont
 }
