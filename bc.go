805a66f3623e2a274908218c7a77b204ebebab1c
Alfredo Vecino <fredovecino@gmail.com>
2019-04-07 18:42:53 -0400

    First working prototype

---
+++ bc.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type rule struct {
+	Regex   string
+	Level   int
+	Message string
+}
+
+var logLevel []string
+
+func main() {
+	//Setting log level
+	logLevel = []string{"Warning"}
+
+	//Command-line flags
+	filePath := flag.String("f", "", "file to check")
+	flag.Parse()
+
+	if *filePath == "" {
+		log.Fatal("No file selected")
+		return
+	}
+
+	checkFile(*filePath)
+}
+
+func ruleParser(filePath string) []rule {
+	fOpen, _ := os.Open(filePath)
+	defer fOpen.Close()
+
+	regexRule := regexp.MustCompile(`(?:regex= ).*?(?: level=)`)
+	levelRule := regexp.MustCompile(`(?:level= ).*?(?: msg=)`)
+	msgRule := regexp.MustCompile(`(?:msg= )(.*)`)
+
+	parsedRules := []rule{}
+
+	fscanner := bufio.NewScanner(fOpen)
+	for fscanner.Scan() {
+		rawText := fscanner.Text()
+
+		regexMatch := strings.TrimSuffix(strings.TrimPrefix(regexRule.FindString(rawText), "regex= "), " level=")
+		levelRaw := strings.TrimSuffix(strings.TrimPrefix(levelRule.FindString(rawText), "level= "), " msg=")
+		msgMatch := strings.TrimPrefix(msgRule.FindString(rawText), "msg= ")
+
+		levelMatch, err := strconv.Atoi(levelRaw)
+		if err != nil {
+			log.Fatal("Fail ATOI")
+		}
+		newRule := rule{
+			Regex:   regexMatch,
+			Level:   levelMatch,
+			Message: msgMatch,
+		}
+		parsedRules = append(parsedRules, newRule)
+	}
+
+	return parsedRules
+}
+
+func checkFile(filename string) {
+	extension := getFileExtension(filename)
+
+	//load rules
+	rules := ruleParser("rules/" + extension + ".rule")
+
+	//load file content
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	cont := 0
+	for i := 0; i < len(rules); i++ {
+		re := regexp.MustCompile(string(rules[i].Regex))
+		matches := re.FindAllIndex(content, -1)
+		for j := 0; j < len(matches); j++ {
+			cont++
+			line := findLine(content, matches[j][0])
+			fmt.Printf("%s:%d: %s: %s\n", filename, line, logLevel[rules[i].Level], rules[i].Message)
+		}
+	}
+	fmt.Printf("total: %d warnings", cont)
+}
+
+//Finds the line of a given index in a string
+func findLine(str []byte, index int) int {
+	cont := 1
+	for i := 0; i < index; i++ {
+		if str[i] == 10 {
+			cont++
+		}
+	}
+	return cont
+}
+
+//Gets the file extension from a filename
+func getFileExtension(filePath string) string {
+	split := strings.Split(filePath, `\`)
+	lastIndex := len(split) - 1
+	filename := split[lastIndex]
+	return strings.Split(filename, ".")[1]
+}
+
+//Reads a file content
+func readFileContent(filePath string) ([]byte, error) {
+	fileRead, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Print(err)
+		return []byte(""), err
+	}
+	return fileRead, nil
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,16 @@
 # Better Code
-This tool is designed to check your code and warn you about bad practices
+This tool is designed to check your code and warn you about bad practices. It uses regular expressions to find patterns of bad code and outputs a log with a message of what's wrong.
+
+There is a .rule file for every language that contains every pattern to check, since all the rules to check are exposed in files you can create your own custom rules to check patterns that you don't want in your code. Rules are matched through the file extension, for a file main.**js** the program will try to find a js.rule to load the patterns.
+
+# How to use
+Just run the executable through the console specifying the file path
+````
+$ bc -d {filepath}
+````
+
+# Custom rules
+Rules are composed of a regular expression, a level in the log and a message. You can create a new rule with the following format:
+````
+regex= {Your regular expression} level= 0 msg= {Message to show when a match is found}
+````
